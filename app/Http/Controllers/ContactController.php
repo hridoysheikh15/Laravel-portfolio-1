e49a3bad127bf9d729443b02b201c64b5fe8d798
hridoysheikh15 <ridoysheikh15@gmail.com>
2024-09-08 01:48:02 +0600

    Add documentation as Documentation.txt

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -12,9 +12,3 @@ class ContactController extends Controller
 
     }
 }
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/hridoysheikh15/Laravel-portfolio-1.git
-git push -u origin main
